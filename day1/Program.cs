753f84814b579316bcaa05b6f0a309534da1b511
Rokas Tamulevičius <rokas.tam@gmail.com>
2020-12-02 22:08:45 +0200

    day1 challenge

---
+++ day1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.StreamReader file = new System.IO.StreamReader("input.txt");
+            string line;
+
+            var inputList = new List<int>();
+            while ((line = file.ReadLine()) != null)
+            {
+                inputList.Add(Int32.Parse(line));
+            }
+
+            foreach (var item in inputList)
+            {
+                foreach (var item2 in inputList)
+                {
+                    foreach (var item3 in inputList)
+                    {
+                        var result = item + item2 + item3;
+                        if (result == 2020)
+                        {
+                            Console.WriteLine(item * item2 * item3);
+                            Console.ReadLine();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
